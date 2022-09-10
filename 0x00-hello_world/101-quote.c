09010979d9619c49be3788ce3c6c76774f0fdd1c
Drimescodes <olamidebamigbola@student.oauife.edu.ng>
2022-09-10 15:25:16 +0000

    Hello World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+<<<<<<< HEAD
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+=======
+	write(2,"and that piece of art is useful\" -Dora Korper, 2015-10-19\n", 59);
+>>>>>>> 5a4ea96da9f2d6ab1c0bf39dbb15cf26ca9a2699
+	return (1);
+}
