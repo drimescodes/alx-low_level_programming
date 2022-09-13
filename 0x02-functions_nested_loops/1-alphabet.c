4b1975fc03dbf5dd0b65f08c232223414ea51e6b
Drimescodes <olamidebamigbola@student.oauife.edu.ng>
2022-09-13 15:27:59 +0000

    Functions-and-nested-loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet of letters
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# alx-low_level_programming
-Introduction to C
