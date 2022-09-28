282e2dcf291481c5d2ba4917e9ec93de9d8dc32e
Drimescodes <olamidebamigbola@student.oauife.edu.ng>
2022-09-28 21:48:07 +0000

    recursion

--- 0x06-pointers_arrays_strings/drimes.c
+++ 0x06-pointers_arrays_strings/drimes.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - check the code
@@ -8,11 +8,15 @@
  */
 int main(void)
 {
-    char s1[98];
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
     char *ptr;
 
-    ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = strcat(s1, s2);
     printf("%s", s1);
+    printf("%s", s2);
     printf("%s", ptr);
     return (0);
 }
